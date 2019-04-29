84fb9ee125cf5efb0506615d71ec87c6fffc54a0
Oleg Bedrin <numeralhuman@gmail.com>
2019-04-29 19:17:00 +0300

    fix the second module

--- API.py
+++ API.py
@@ -43,4 +43,4 @@ class Facade(Unimplementable):
         return self.models['probability_module'].get_probability(high, low, second, minute, hour, week_day, week_number, month, time)
 
     def get_cost(self, time):
-        return self.models['cost_module'].get_price(time)
+        return self.models['cost_module'].get_cost(time)
